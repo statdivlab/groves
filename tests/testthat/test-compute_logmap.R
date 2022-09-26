4fa1e2c26f363e6e62969f7b1e3f8d064ab803ec
Sarah Teichman <teichs@uw.edu>
2022-09-26 15:05:58 -0700

    adding test for try error

--- tests/testthat/test-compute_logmap.R
+++ tests/testthat/test-compute_logmap.R
@@ -41,3 +41,18 @@ test_that("compute_logmap gives error if non-numeric base_lab isn't in tree_name
              calculate a base tree for you, input a number corresponding your desired
              base tree's position in tree_paths, or a name that appears in tree_paths.")
 })
+
+test_that("compute_logmap gives error if base tree is on boundary", {
+  path <- paste0(system.file("txt", package = "groves"), "/")
+  tree_paths <- paste0(path, "tree", 1:3, ".txt")
+  tree_paths <- c(tree_paths, paste0(path, "boundary_tree.txt"))
+  expect_error(compute_logmap(tree_paths = tree_paths,
+                              base_lab = "boundary_tree",
+                              tree_names = c("tree1", "tree2", "tree3", 
+                                             "boundary_tree")),
+               "The chosen base tree, boundary_tree, is on the boundary of tree space and the log
+                map cannot be computed using this tree. If you are manually choosing the
+                base tree, please try another one. If this base tree was chosen automatically,
+                you can try setting a base tree manually, but if you are unable to find a tree
+                that does not return this error, this method may not work for your tree set.")
+})
