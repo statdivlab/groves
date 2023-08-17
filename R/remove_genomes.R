8c3d0e57ce3f939c90ea1f0dafdbf5669803a118
Sarah Teichman <teichs@uw.edu>
2023-08-17 16:35:47 -0700

    updating remove_genomes function to make a new folder if you want

--- R/remove_genomes.R
+++ R/remove_genomes.R
@@ -4,6 +4,7 @@
 #' @param target_genes A list of gene names for alignment files to remove empty lines from.
 #' @param target_genomes A list of genome names to remove from alignment files
 #' @param path Path to the alignment files.
+#' @param new_folder_path Path to an optional new folder to put alignments in. 
 #' @param tail Ending of alignment files.
 #' @param new_file_suffix A suffix to amend to alignment files after empty lines are removed.
 #' By default this is "", which means that the new file will replace the old file.
@@ -18,7 +19,7 @@
 #'
 #' @export
 remove_genomes <- function(target_genes, target_genomes, path = "", tail = ".fa",
-                           new_file_suffix = "") {
+                           new_file_suffix = "", new_folder_path = "") {
   genomes_remove <- paste0(">", target_genomes)
   for (i in 1:length(target_genes)) {
     fasta <- paste0(path, target_genes[i], tail)
@@ -27,7 +28,11 @@ remove_genomes <- function(target_genes, target_genomes, path = "", tail = ".fa"
     if (length(missing_ind) > 0) {
       rm_ind <- c(missing_ind, missing_ind + 1)
       fasta_miss_rm <- fasta_contents[-rm_ind]
-      fileConn <- file(paste0(path, target_genes[i], new_file_suffix, tail))
+      if (new_folder_path == "") {
+        fileConn <- file(paste0(path, target_genes[i], new_file_suffix, tail))
+      } else {
+        fileConn <- file(paste0(new_folder_path, target_genes[i], new_file_suffix, tail))
+      }
       writeLines(fasta_miss_rm, fileConn)
       close(fileConn)
     }
